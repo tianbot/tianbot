b4526a04e4558a8379b8cda550f8883a5f5bcf3b
Tian Bo <tianb03@hotmail.com>
2022-06-06 10:32:37 +0800

    zed support multi. completed.

--- tianbot_bringup/install/install_udev.sh
+++ tianbot_bringup/install/install_udev.sh
@@ -3,8 +3,7 @@
 echo "Tianbot Udev Rules Updating..."
 echo "Check it using the command : ls -l /dev | grep tianbot"
 
-sudo rm /etc/udev/rules.d/99-tianbot-base.rules /etc/udev/rules.d/99-tianbot-gps.rules /etc/udev/rules.d/99-tianbot-rplidar.rules /etc/udev/rules.d/99-tianbot-joystick.rules /etc/udev/rules.d/99-tianbot-usb-cam.rules /etc/udev/rules.d/99-tianbot-tom.rules
-sudo cp ./_udev_/*.rules  /etc/udev/rules.d
+sudo cp -f ./_udev_/*.rules  /etc/udev/rules.d
 
 echo " "
 echo "Restarting udev"
